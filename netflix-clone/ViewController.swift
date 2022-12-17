c1e6106aef582e4c1e8350bbe846cea3397f62f6
Vhsxuz <andreas.alexander991@gmail.com>
2022-12-17 14:02:26 +0700

    Initial Commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by Andreas Alexander on 17/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
